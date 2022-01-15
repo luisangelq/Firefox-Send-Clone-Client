080444c6ea2e14024c807a34010245f7a1c63329
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2022-01-14 21:23:54 -0600

    change on url server

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    backendURL: "https://blooming-waters-69865.herokuapp.com",
+    backendURL: "https://firefox-send-clone-server.herokuapp.com",
     frontendURL: "https://firefox-send-clone.netlify.app",
   },
 };
